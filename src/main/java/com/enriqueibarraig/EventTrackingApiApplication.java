b7694fadd2455dfdf44bc1b38428093125d536bd
Enrique Ibarra <jibarra1617@gmail.com>
2026-07-27 23:33:09 -0700

    Initial project setup

---
+++ src/main/java/com/enriqueibarraig/EventTrackingApiApplication.java
@@ -0,0 +1,13 @@
+package com.enriqueibarraig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTrackingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTrackingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enriqueibarraig/EventTrackingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enriqueibarraig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTrackingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
